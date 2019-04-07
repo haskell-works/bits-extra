2a7fdef3a15f6851362605a240b0683a4b7c92c1
John Ky <newhoggy@gmail.com>
2019-04-08 08:36:53 +1000

    Fix hlint annotation

--- test/Data/Bits/PdepSpec.hs
+++ test/Data/Bits/PdepSpec.hs
@@ -15,7 +15,7 @@ import qualified Hedgehog.Range as R
 
 {-# ANN module ("HLint: ignore Redundant do"        :: String) #-}
 {-# ANN module ("HLint: ignore Reduce duplication"  :: String) #-}
-{-# ANN module ("HLint: redundant bracket"          :: String) #-}
+{-# ANN module ("HLint: ignore Redundant bracket"   :: String) #-}
 
 spec :: Spec
 spec = describe "Data.Bits.PdepSpec" $ do
